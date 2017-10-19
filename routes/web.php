e9ce97025a787f06bea89cc2d42b5fdf8b12095a
chenyirou <annie851216@gmail.com>
2017-10-19 18:32:31 +0800

    建立route:get('/home')

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
+
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
