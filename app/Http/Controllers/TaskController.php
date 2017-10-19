10961c359892dcb74fe62988e847bd0252ba632c
chenyirou <annie851216@gmail.com>
2017-10-19 18:22:16 +0800

    在TaskController中建立auth 中介層

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * 建立一個新的控制器實例。
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
